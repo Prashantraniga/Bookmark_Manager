fff2f503f1f1e2a8134e819a81cdf370412c2ddf
Prashantraniga <prashant.raniga2012@gmail.com>
2018-10-24 15:35:52 +0100

    Wednesday afternoon commit task 9

--- app.rb
+++ app.rb
@@ -13,7 +13,16 @@ class Bookmark_Manager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-    erb :bookmarks
+    erb :"bookmarks"
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params['url'])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
